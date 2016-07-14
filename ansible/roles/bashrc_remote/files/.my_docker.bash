dd86de826515d84f09e2c5c58dbec55d0e911ee8
bwnyasse <risbonyasse@gmail.com>
2016-07-14 14:53:31 +0200

    Update docker clean function

--- ansible/roles/bashrc_remote/files/.my_docker.bash
+++ ansible/roles/bashrc_remote/files/.my_docker.bash
@@ -4,10 +4,14 @@ alias d='docker'
 alias dc='docker-compose'
 alias dm='docker-machine'
 
-dcleanup() {
+removeDanglingImages() {
   # Clean up exited container and dangling images
    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+}
+
+removeOldImages() {
+    removeDanglingImages
 
    # removes old from months and week
    docker images | grep 'months ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
