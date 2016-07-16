989bf912ea3a9a4e480ab0a7c2d65acf2341ab7b
bwnyasse <risbonyasse@gmail.com>
2016-07-16 21:16:47 +0200

    Add docker clean everything mechanism

--- ansible/roles/bashrc_remote/files/.my_docker.bash
+++ ansible/roles/bashrc_remote/files/.my_docker.bash
@@ -4,9 +4,13 @@ alias d='docker'
 alias dc='docker-compose'
 alias dm='docker-machine'
 
+removeExitedContainer() {
+   docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+}
+
 removeDanglingImages() {
   # Clean up exited container and dangling images
-   docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+    removeExitedContainer
    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
 
@@ -16,4 +20,11 @@ removeOldImages() {
    # removes old from months and week
    docker images | grep 'months ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
    docker images | grep 'weeks ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
+}
+
+dockerCleanEverything() {
+    # Delete all containers
+    docker rm $(docker ps -a -q)
+    # Delete all images
+    docker rmi $(docker images -q)
 }
