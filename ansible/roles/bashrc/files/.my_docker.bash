17c3ca416cce606df19633c7ea4429a17d0164d6
bwnyasse <risbonyasse@gmail.com>
2016-07-24 22:17:27 +0200

    Update docker alias utility functions

--- ansible/roles/bashrc/files/.my_docker.bash
+++ ansible/roles/bashrc/files/.my_docker.bash
@@ -8,21 +8,21 @@ removeExitedContainer() {
    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
 }
 
-removeDanglingImages() {
+dockerClean() {
   # Clean up exited container and dangling images
-    removeExitedContainer
+   removeExitedContainer
    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
 
-removeOldImages() {
-    removeDanglingImages
+dockerRemoveOldImages() {
+    dockerClean
 
    # removes old from months and week
    docker images | grep 'months ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
    docker images | grep 'weeks ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
 }
 
-dockerCleanEverything() {
+dddockerCleanEverything() {
     # Delete all containers
     docker rm $(docker ps -a -q)
     # Delete all images
