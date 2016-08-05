84eda765cab08d2832cd2a058f4db8a896c2aaff
bwnyasse <risbonyasse@gmail.com>
2016-08-05 22:55:54 +0200

    Update docker bash alias function

--- ansible/roles/bashrc/files/.my_docker.bash
+++ ansible/roles/bashrc/files/.my_docker.bash
@@ -5,24 +5,27 @@ alias dc='docker-compose'
 alias dm='docker-machine'
 
 removeExitedContainer() {
+  # Clean up exited container
    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-}
 
-dockerClean() {
-  # Clean up exited container and dangling images
-   removeExitedContainer
+   # Remove Dangling images
    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
 
+removeDanglingVolumes() {
+    docker volume ls -qf dangling=true | xargs -r docker volume rm
+}
+
 dockerRemoveOldImages() {
-    dockerClean
+    removeExitedContainer
+    removeDanglingVolumes
 
    # removes old from months and week
    docker images | grep 'months ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
    docker images | grep 'weeks ago' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
 }
 
-dddockerCleanEverything() {
+removeResetHeard() {
     # Delete all containers
     docker rm $(docker ps -a -q)
     # Delete all images
