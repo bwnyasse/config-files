f8e0dfe38420d2f37266b0279949ad6a46b9b148
bwnyasse <risbonyasse@gmail.com>
2016-08-06 14:22:54 +0200

    Update docker role

--- ansible/playbooks/local-xubuntu.sh
+++ ansible/playbooks/local-xubuntu.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ansible-playbook --vault-password-file=~/.ansible_pwd -vvvv -i ../servers/hosts  \
-     -K -u bwnyasse local-xubuntu.yml
+     -K --ask-become-pass -u bwnyasse local-xubuntu.yml
--- ansible/roles/bashrc/files/.my_docker.bash
+++ ansible/roles/bashrc/files/.my_docker.bash
@@ -7,7 +7,7 @@ alias dm='docker-machine'
 removeExitedContainer() {
   # Clean up exited container
    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-
+   docker rm -v $(docker ps --filter status=created -q 2>/dev/null) 2>/dev/null
    # Remove Dangling images
    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
