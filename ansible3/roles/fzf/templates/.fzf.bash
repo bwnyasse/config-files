a25103ffc3fcdb45b87372893bac8dada1855909
bwnyasse <risbonyasse@gmail.com>
2017-04-25 23:28:00 +0200

    Add fzf to config mgt

---
+++ ansible3/roles/fzf/templates/.fzf.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == *{{ user.home}}/.fzf/bin* ]]; then
+  export PATH="$PATH:{{ user.home}}/.fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" == *{{ user.home}}/.fzf/man* && -d "{{ user.home}}/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:{{ user.home}}/.fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "{{ user.home}}/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "{{ user.home}}/.fzf/shell/key-bindings.bash"
